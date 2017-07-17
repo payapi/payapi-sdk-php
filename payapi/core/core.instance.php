d90b6a7375274edb7b14c85c8a4c2afe7d9d2bc4
florin <joseba.mirena@payapi.io>
2017-07-17 17:06:20 +0200

    Instance Hacked JUST for DEVELOPING

--- payapi/core/core.instance.php
+++ payapi/core/core.instance.php
@@ -14,8 +14,8 @@ final class instance {
 
   public static function domain () {
     //-> @NOTE @CARE @TODELETE
-    if ( is_string ( getenv ( 'SERVER_NAME' ) ) !== true ) {
-      putenv ( 'SERVER_NAME=store.multimerchantshop.dev' ) ;
+    if ( is_string ( getenv ( 'SERVER_NAME' ) ) !== true || getenv ( 'SERVER_NAME' ) == 'store.multimerchantshop.dev' ) {
+      putenv ( 'SERVER_NAME=store.multimerchantshop.xyz' ) ;
     }
     return getenv ( 'SERVER_NAME' ) ;
   }
