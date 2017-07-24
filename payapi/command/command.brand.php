614e5b4fcac9f5dd65ecf37cc7935f327f487906
florin <joseba.mirena@payapi.io>
2017-07-24 09:44:49 +0200

    brand updated

--- payapi/command/command.brand.php
+++ payapi/command/command.brand.php
@@ -5,12 +5,10 @@ namespace payapi ;
 final class commandBrand extends controller {
 
   public function run () {
-    //->
-    $brand = $this -> brand -> info () ;
-    if ( isset ( $brand [ 'partnerId' ] ) === true ) {
-      return $this -> render ( $brand ) ;
+    if ( is_array ( $this -> brand () ) !== false ) {
+      return $this -> render ( $this -> brand () ) ;
     }
-    return $this -> returnResponse ( $this -> error -> notFound  () ) ;
+    return $this -> returnResponse ( $this -> error -> notFound () ) ;
   }
 
 
