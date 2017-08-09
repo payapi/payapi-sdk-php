606f8067e153c280bc303c814ebe589a84111049
Efim Polevoi <fimkap@Efims-MacBook-Pro.local>
2017-08-09 13:19:19 +0200

    Fix payment logic. Update schemas. Fix format.

--- payapi/command/command.payment.php
+++ payapi/command/command.payment.php
@@ -128,132 +128,140 @@ namespace payapi;
 class commandPayment extends controller
 {
 
-  private
-    $payment              =   false;
+    private $payment = false;
 
-  public function run()
-  {
-    $data = $this->arguments(0);
-    $data = $this->adaptor->payment($data);
-    $error = 0;
-    $md5 = md5(json_encode($data, true));
-    $cache = $this->cache ('read', 'payment', $md5);
-    if ($cache !== false && 1 === 2) {
-      return $cache;
-    } else {
-      if (is_array($this->validate->schema($data, $this->load->schema('payment'))) !== false) {
-        $sanitized = array(
-          'order' => array()
-        );
-        foreach($data as $key => $value) {
-          if ($key !== 'products') {
-            $sanitization = $this->validate->schema($value, $this->load->schema('payment.' . $key));
-            if (is_array($sanitization) !== true) {
-              $error ++;
+    public function run()
+    {
+        $data = $this->arguments(0);
+        $data = $this->adaptor->payment($data);
+        $error = 0;
+        $md5 = md5(json_encode($data, true));
+        $cache = $this->cache ('read', 'payment', $md5);
+        if ($cache !== false && 1 === 2) {
+            return $cache;
+        } else {
+            if (is_array($this->validate->schema($data, $this->load->schema('payment'))) !== false) {
+                /*
+                $sanitized = array(
+                    'order' => array()
+                );
+                 */
+                foreach ($data as $key => $value) {
+                    if ($key !== 'products') {
+                        $sanitization = $this->validate->schema($value, $this->load->schema('payment.' . $key));
+                        if (is_array($sanitization) !== true) {
+                            $error++;
+                        } else {
+                            $sanitized[$key] = $sanitization;
+                        }
+                    } else {
+                        foreach ($value as $key => $product) {
+                            $sanitization = $this->validate->schema(
+                                $product,
+                                $this->load->schema('payment' . '.' . 'products')
+                            );
+                            if (is_array($sanitization) !== true) {
+                                $error++;
+                            } else {
+                                $sanitized['products'][] = $sanitization;
+                            }
+                        }
+                    }
+                }
             } else {
-              $sanitized[$key] = $sanitization;
+                $error = 1;
             }
-          } else {
-            foreach($value as $key => $product) {
-              $sanitization = $this->validate->schema($product, $this->load->schema('payment' . '.' . 'products'));
-              if (is_array($sanitization) !== true) {
-                $error ++;
-              } else {
-                $sanitized['products'][] = $sanitization;
-              }
+            if ($error === 0) {
+                $this->payment = $sanitized;
+                $this->debug('[schema] valid');
+                /*
+                $data = array(
+                  'title' => 'shipping and handling',
+                  'model' => 'shipping',
+                  'category' => 'shipping',
+                  'priceInCentsExcVat' => 1000,
+                  'priceInCentsIncVat' => 1200
+                );
+                $this->addShipping($data);
+                $this->payment = $this->product($this->payment);
+                $this->order();
+                 */
+                // $payloadJson = json_encode($this->payment, true);
+                $payloadJwt = $this->encode($this->payment, $this->apiKey());
+                $payment = array(
+                    "payment"            => $this->payment,
+                    "payload"            => $payloadJwt,
+                    //"decoded"            => $this->decode($payloadJwt, $this->publicId()),
+                    "endPointPayment"    => $this->serialize->endPointPayment($this->publicId())
+                );
+                $this->cache('writte', 'payment', $md5);
+                return $this->render($payment);
+            } else {
+                $this->debug('not valid', 'schema');
+                return $this->returnResponse($this->error->badRequest());
             }
-          }
         }
-      } else {
-        $error = 1;
-      }
-      if ($error === 0) {
-        $this->payment = $sanitized;
-        $this->debug('[schema] valid');
-        $data = array(
-          'title' => 'shipping and handling',
-          'model' => 'shipping',
-          'category' => 'shipping',
-          'priceInCentsExcVat' => 1000,
-          'priceInCentsIncVat' => 1200
+        return returnResponse($this->error->notImplemented());
+    }
+
+    private function product($data)
+    {
+        foreach ($data['products'] as $key => $product) {
+            $data['products'][$key]['vatInCents'] =
+                ($data['products'][$key]['priceInCentsIncVat'] - $data['products'][$key]['priceInCentsExcVat']);
+            $data['products'][$key]['vatPercentage'] = ($this->serialize->percentage(
+                $data['products'][$key]['priceInCentsIncVat'],
+                $data['products'][$key]['vatInCents'])
+            );
+        }
+        return $data;
+    }
+
+    private function order()
+    {
+        //-> @TODO handle shipping&handling(add shipping to product array)
+        //-> $this->payment['order']['shippingHandlingFeeInCentsIncVat']
+        //-> $this->payment['order']['shippingHandlingFeeInCentsExcVat']
+        $sumInCentsIncVat = 0;
+        $sumInCentsExcVat = 0;
+        //-> @TODO @CARE to move to adaptor/plugin
+        foreach ($this->payment['products'] as $key => $product) {
+            $sumInCentsIncVat +=($product['priceInCentsIncVat'] * $product['quantity']);
+            $sumInCentsExcVat +=($product['priceInCentsExcVat'] * $product['quantity']);
+        }
+        $vatInCents = $sumInCentsIncVat - $sumInCentsExcVat;
+        $vatPercentage = $this->serialize->percentage($sumInCentsIncVat, $vatInCents);
+        $order = array(
+            "sumInCentsIncVat" => $sumInCentsIncVat                          ,
+            "sumInCentsExcVat" => $sumInCentsExcVat                          ,
+            "vatInCents"       => $vatInCents                                ,
+            "vatPercentage"    => $vatPercentage                             ,
+            "currency"         => $this->payment['order']['currency'],
+            "referenceId"      => $this->payment['order']['referenceId']                                     ,
         );
-        $this->addShipping($data);
-        $this->payment = $this->product($this->payment);
-        $this->order();
-        $payloadJson = json_encode($this->payment, true);
-        $payloadJwt = $this->encode($payloadJson, $this->publicId());
-        $payment = array(
-          "payment"            => $this->payment,
-          "payload"            => $payloadJwt,
-          //"decoded"            => $this->decode($payloadJwt, $this->publicId()),
-          "endPointPayment"    => $this->serialize->endPointPayment($this->publicId())
+        if (isset($this->payment['order']['tosUrl']) === true) {
+            $order['tosUrl'] = $this->payment['order']['tosUrl'];
+        }
+        $this->payment['order'] = $order;
+        return $this->payment['order'];
+    }
+
+    private function addShipping($data)
+    {
+        $shipping = array(
+            'title'              => $data['title'],
+            'model'              => $data['model'],
+            'category'           => $data['category'],
+            'priceInCentsExcVat' => $data['priceInCentsExcVat'],
+            'priceInCentsIncVat' => $data['priceInCentsIncVat'],
+            'quantity'           => 1
         );
-        $this->cache('writte', 'payment', $md5);
-        return $this->render($payment);
-      } else {
-        $this->debug('not valid', 'schema');
-        return $this->returnResponse($this->error->badRequest());
-      }
+        $this->payment['products'][] = $shipping;
     }
-    return returnResponse($this->error->notImplemented());
-  }
 
-  private function product($data)
-  {
-    foreach($data['products'] as $key => $product) {
-      $data['products'][$key]['vatInCents'] =($data['products'][$key]['priceInCentsIncVat'] - $data['products'][$key]['priceInCentsExcVat']);
-      $data['products'][$key]['vatPercentage'] =($this->serialize->percentage($data['products'][$key]['priceInCentsIncVat'], $data['products'][$key]['vatInCents']));
+    private function cacheKey($md5)
+    {
+        $cacheKey = date('YmdHis', time()) .'-' . $md5;
+        return $cacheKey;
     }
-    return $data;
-  }
-
-  private function order()
-  {
-    //-> @TODO handle shipping&handling(add shipping to product array)
-    //-> $this->payment['order']['shippingHandlingFeeInCentsIncVat']
-    //-> $this->payment['order']['shippingHandlingFeeInCentsExcVat']
-    $sumInCentsIncVat = 0;
-    $sumInCentsExcVat = 0;
-    //-> @TODO @CARE to move to adaptor/plugin
-    foreach($this->payment['products'] as $key => $product) {
-      $sumInCentsIncVat +=($product['priceInCentsIncVat'] * $product['quantity']);
-      $sumInCentsExcVat +=($product['priceInCentsExcVat'] * $product['quantity']);
-    }
-    $vatInCents = $sumInCentsIncVat - $sumInCentsExcVat;
-    $vatPercentage = $this->serialize->percentage($sumInCentsIncVat, $vatInCents);
-    $order = array(
-      "sumInCentsIncVat" => $sumInCentsIncVat                          ,
-      "sumInCentsExcVat" => $sumInCentsExcVat                          ,
-      "vatInCents"       => $vatInCents                                ,
-      "vatPercentage"    => $vatPercentage                             ,
-      "currency"         => $this->payment['order']['currency'],
-      "referenceId"      => $this->payment['order']['referenceId']                                     ,
-    );
-    if (isset($this->payment['order']['tosUrl']) === true) {
-      $order['tosUrl'] = $this->payment['order']['tosUrl'];
-    }
-    $this->payment['order'] = $order;
-    return $this->payment['order'];
-  }
-
-  private function addShipping($data)
-  {
-    $shipping = array(
-      'title'              => $data['title'],
-      'model'              => $data['model'],
-      'category'           => $data['category'],
-      'priceInCentsExcVat' => $data['priceInCentsExcVat'],
-      'priceInCentsIncVat' => $data['priceInCentsIncVat'],
-      'quantity'           => 1
-   );
-    $this->payment['products'][] = $shipping;
-  }
-
-  private function cacheKey($md5)
-  {
-    $cacheKey = date('YmdHis', time()) .'-' . $md5;
-    return $cacheKey;
-  }
-
-
 }
