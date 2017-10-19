ecadb59585d3e76f2313f551c3b96bdadaa7f211
florin <florin@payapi.io>
2017-10-19 11:25:09 +0200

    delete account cache in settings call

--- src/payapi/command/command.settings.php
+++ src/payapi/command/command.settings.php
@@ -76,6 +76,7 @@ final class commandSettings extends controller
 
   public function run()
   {
+    $this->cache('delete', 'settings', $this->instance());
     if ($this->validate->publicId($this->arguments(1)) === true && $this->validate->apiKey($this->arguments(2)) === true) {
       $publicId = $this->arguments(1);
       $apiKey = $this->arguments(2);
