03e5ff083dd62ade487d2070a1402ce867a7a1d7
Efim Polevoi <fimkap@Efims-MacBook-Pro.local>
2017-11-30 12:25:21 +0100

    Add attributes support.

--- src/payapi/plugin/plugin.oscommerce.php
+++ src/payapi/plugin/plugin.oscommerce.php
@@ -67,10 +67,17 @@ final class plugin
                 $qty     = intval($item['qty']);
                 $totalTaxes += $qty * $taxes;
                 
+                // Set attributes
+                $attributes = '';
+                foreach ($item['attributes'] as $attribute) {
+                    $attributes .= ' ' . $attribute['option'] . ': ' . $attribute['value'];
+                }
+                error_log('attributes: '.$attributes, 0);
+                
                 $products[] = [
                     "id"                 => $item['id'],
                     "quantity"           => $qty,
-                    "title"              => $item['name'],
+                    "title"              => $item['name'] . $attributes,
                     "model"              => $item['model'],
                     "priceInCentsIncVat" => round($incVat * 100),
                     "priceInCentsExcVat" => round($excVat * 100),
@@ -87,7 +94,7 @@ final class plugin
                 array('extraData' => serialize($tmp_order))
             );
 
-            error_log(' products at index 0: '.json_encode($products[0]), 0);
+            // error_log(' products at index 0: '.json_encode($products[0]), 0);
         }
 
         $shipExcVat  = $tmp_order->info['shipping_cost'];
@@ -152,115 +159,6 @@ final class plugin
             ];
 
         return $res;
-
-        /*
-        $currency = new \Currency($cart->id_currency);
-
-        $sumInCentsIncVat = (float)$cart->getOrderTotal(true, \Cart::BOTH) * 100;
-        $sumInCentsExcVat = (float)$cart->getOrderTotal(false, \Cart::BOTH) * 100;
-
-        $vatInCents = $sumInCentsIncVat - $sumInCentsExcVat;
-
-        $referenceId = (string)$cart->id;
-
-        // Terms of Services
-        $tosUrl = "https://payapi.io/terms";
-
-        // Assume the schema will take care of empty/null payment method
-        $order = array( 'sumInCentsIncVat' => $sumInCentsIncVat,
-            'sumInCentsExcVat' => $sumInCentsExcVat,
-            'vatInCents' => $vatInCents,
-            'currency' => $currency->iso_code,
-            'referenceId' => $referenceId,
-            'tosUrl' => $tosUrl,
-        );
-        if ($partialPaymentMethod) {
-            $order = array_merge($order, array('preselectedPartialPayment' => $partialPaymentMethod));
-        }
-
-        $products_array = array();
-        foreach ($cart->getProducts() as $product) {
-            // Price calculations
-            $prodPriceInCentsExcVat = round($product['price'] * 100);
-            $prodPriceInCentsIncVat = round($product['price_with_reduction'] * 100);
-            $prodVatInCents = $prodPriceInCentsIncVat - $prodPriceInCentsExcVat;
-            $prodVatPercentage = round($prodVatInCents / $prodPriceInCentsExcVat * 100);
-
-            //error_log("attributes: ".$product['attributes'], 0);
-            $name_with_attributes = $product['name']." ".$product['attributes'];
-
-            $productObject = array(
-                'id' => $product['id_product'],
-                'quantity' => $product['quantity'],
-                'title' => $name_with_attributes,
-                'description' => $product['description_short'],
-                'category' => $product['category'],
-                'priceInCentsIncVat' => $prodPriceInCentsIncVat,
-                'priceInCentsExcVat' => $prodPriceInCentsExcVat,
-                'vatInCents' => $prodVatInCents,
-                'vatPercentage' => $prodVatPercentage,
-            );
-            array_push($products_array, $productObject);
-        }
-
-        $shipping_cost = (float)$cart->getOrderTotal(true, \Cart::ONLY_SHIPPING) * 100;
-        $shipping_cost_no_tax = (float)$cart->getOrderTotal(false, \Cart::ONLY_SHIPPING) * 100;
-        // Add shipping cost as a last product
-        $shippingObject = array(
-            'id' => $cart->id_carrier,
-            'quantity' => 1,
-            'title' => "Shipping cost",
-            'description' => "",
-            'category' => "shipping",
-            'priceInCentsIncVat' => $shipping_cost,
-            'priceInCentsExcVat' => $shipping_cost_no_tax,
-            'vatInCents' => 0,
-            'vatPercentage' => 0,
-        );
-        array_push($products_array, $shippingObject);
-
-        // Get shipping address info.
-        $address = new \Address((int)$cart->id_address_delivery);
-
-        $customer = new \Customer((int)$address->id_customer);
-
-        $country = new \Country((int)$address->id_country);
-
-        $shipping_address = array(
-            'recipientName' => $customer->firstname." ".$customer->lastname,
-            'co' => $address->company,
-            'streetAddress' => $address->address1,
-            'streetAddress2' => $address->address2,
-            'postalCode' => $address->postcode,
-            'city' => $address->city,
-            'countryCode' => $country->iso_code,
-        );
-
-        $shop_url = \Tools::getHttpHost(true).__PS_BASE_URI__;
-
-        $returnUrls = array(
-            'success' => $shop_url.'index.php?payapireturn=success',
-            'cancel' => $shop_url.'index.php?payapireturn=cancel',
-            'failed' => $shop_url.'index.php?payapireturn=failed',
-        );
-
-        $callbacks = array(
-            'processing' => $shop_url.'index.php?fc=module&module=payapi&controller=callback',
-            'success' => $shop_url.'index.php?fc=module&module=payapi&controller=callback',
-            'failed' => $shop_url.'index.php?fc=module&module=payapi&controller=callback',
-            'chargeback' => $shop_url.'index.php?fc=module&module=payapi&controller=callback',
-        );
-
-        $secureformObject = array(
-            'order' => $order,
-            'products' => $products_array,
-            'shippingAddress' => $shipping_address,
-            'callbacks' => $callbacks,
-            'returnUrls' => $returnUrls
-        );
-
-        return $secureformObject;
-         */
     }
 
     public function instantPayment($payment)
