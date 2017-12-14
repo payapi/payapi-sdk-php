571181832ada21752bde8c29ab5604321664a738
Joseba Mirena Fernández Florín <contact@josebamirena.com>
2017-12-14 17:44:13 +0100

    added payload command

---
+++ src/payapi/command/command.payload.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace payapi;
+
+/*
+* @COMMAND
+*           $sdk->payload($data)
+*
+* @TYPE     private
+*
+* @RETURNS
+*           encoded payload
+*
+*/
+
+final class commandPayload extends controller
+{
+
+	public function run()
+  	{
+		if ($this->arguments(0) ==! false) {
+			return $this->render($this->payload($this->arguments(0)));
+		}
+		return $this->returnResponse($this->error->badRequest());
+	}
+
+	private function payload($data)
+	{
+		return $this->encode($data, $this->apiKey());
+	}
+	//-> @NOTE not used by now
+	private function merchantPayload()
+	{
+		$partial = $this->partialPayments();
+		if ($this->publicId() !== false && is_array($partial) === true) {
+		    $payload = array(
+		      "storeDomain" => getenv('SERVER_NAME')
+		    );
+		    return $this->payload($payload);
+		}
+		return false;
+	}
+
+
+}
