8d3c207842067393f84a34e23bcd6ce32811739a
Joseba Mirena Fernández Florín <contact@josebamirena.com>
2018-02-23 17:57:38 +0100

    SDK version uopdate

--- src/payapi/app.engine.php
+++ src/payapi/app.engine.php
@@ -7,7 +7,7 @@ final class engine
 
     public static $single = false;
 
-    private $version      = '3.0.0';
+    private $version      = '3.0.2';
     private $plugin       = 'native';
     private $adapt        = false;
     private $debug        = false;
