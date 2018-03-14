1e3d6ce47f394d8db70ad7c82623cce904c4ce3b
Joseba Mirena Fernández Florín <contact@josebamirena.com>
2018-03-14 12:48:53 +0100

    added sdk debug public command

---
+++ src/payapi/command/command.debug.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace payapi;
+
+/*
+* @COMMAND
+*           $sdk->debug(string)
+*
+* @TYPE     public
+*
+* @RETURNS
+*           debugged string || error
+*
+* @SAMPLE
+*          ["code"]=>
+*           int(200)
+*          ["error"]=>
+*           string(15) "debugged string"
+*
+*/
+final class commandDebug extends controller
+{
+    public function run()
+    {
+        if ($this->arguments(0) !== false && is_string($this->arguments(0)) === true) {
+            return $this->render($this->arguments(0));
+        }
+        return $this->returnResponse($this->error->badRequest());
+    }
+
+}
