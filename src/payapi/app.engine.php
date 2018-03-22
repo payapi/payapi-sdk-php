6c79e81b628f58ee37f643f5f1528d244737394b
Joseba Mirena Fernández Florín <contact@josebamirena.com>
2018-03-22 13:06:27 +0100

    updated sdk version

--- src/payapi/app.engine.php
+++ src/payapi/app.engine.php
@@ -7,7 +7,7 @@ final class engine
 
     public static $single = false;
 
-    private $version      = '3.0.7';
+    private $version      = '3.0.8';
     private $plugin       = 'native';
     private $adapt        = false;
     private $debug        = false;
