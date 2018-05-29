17df117c5f3ccbedf24a427f1128bc96717c9559
Joseba Mirena Fernández Florín <contact@josebamirena.com>
2018-05-29 11:27:01 +0200

    fixed

--- hack/hack.server.php
+++ hack/hack.server.php
@@ -11,11 +11,11 @@ $hack_domain = 'payapi.io';
 $hack_ip = '146.148.30.253';
 
 if (is_string(getenv('SERVER_NAME')) !== true) {
-  putenv('SERVER_NAME=' . $domain);
+  putenv('SERVER_NAME=' . $hack_domain);
 }
 if (is_string(getenv('SERVER_HOST')) !== true) {
-  putenv('SERVER_HOST=' . $domain);
+  putenv('SERVER_HOST=' . $hack_domain);
 }
 if (is_string(getenv('REMOTE_ADDR')) !== true) {
-  putenv('REMOTE_ADDR=' . $ip);
+  putenv('REMOTE_ADDR=' . $hack_ip);
 }
